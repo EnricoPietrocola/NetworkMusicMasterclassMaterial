a5f95a48ad97b537f2b3e48e2516db7f75566796
ePietrocola <enricopietrocola@gmail.com>
2025-04-15 23:00:55 +0200

    Adjust url to directly open pagina

--- 1-Http/app.js
+++ 1-Http/app.js
@@ -30,5 +30,5 @@ app.get('/pagina', (req, res) => {
 
 //Avvia il server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}/pagina`);
 });
